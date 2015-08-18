ad09a043cd1ac7a28146b9d938bd350e83c56a21
Duspiwa <gpt1k7@hotmail.com>
2015-08-18 02:36:49 +0200

    post rb storage s3

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
-	has_attached_file :image, styles: {large:"600x600", medium: "300x300>", thumb:"100x100>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :image, styles: {large:"600x600", medium: "300x300>", thumb:"100x100>" }, :default_url => "/images/:style/missing.png":storage => :s3,
+     :bucket => '*****', :s3_credentials => S3_CREDENTIALS
   	validates_attachment_content_type :image, content_type: %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document)
  
 	
